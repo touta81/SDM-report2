5f20fc55b98ec9cf8e5d54b1d6938d197b0ea12f
knkyh13 <knkyh13@gmail.com>
2022-01-22 11:39:39 +0900

    テストケースの作成
    同値分割法に基づきテストケースを作成した

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,25 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (-1, calc(-1,50))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (-1, calc(0.3,50))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (2000,calc(40,50))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (2500,calc(50,50))
 
+        def test_sample5 (self):
+                self.assertEqual (3000,calc(50,50))
+
+        def test_sample6 (self):
+                self.assertEqual (-1,calc(10.5,50))
+
+        def test_sample7 (self):
+                self.assertEqual (-1,calc(999.9,50))
+        
+        def test_sample8 (self):
+                self.assertEqual (-1,calc(1000,50))
